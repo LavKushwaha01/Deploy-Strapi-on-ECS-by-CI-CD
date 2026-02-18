08150d4e5867c182cad1c1b005ccdc42d4615aa4
Lav Kushwaha <lavkumar062@gmail.com>
2026-02-18 13:20:18 +0530

    ecs changed

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -9,8 +9,7 @@ resource "aws_ecs_task_definition" "strapi" {
   cpu                      = "512"
   memory                   = "1024"
 
-  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
-
+  execution_role_arn = data.aws_iam_role.ecs_execution_role.arn
 
   container_definitions = jsonencode([
     {
