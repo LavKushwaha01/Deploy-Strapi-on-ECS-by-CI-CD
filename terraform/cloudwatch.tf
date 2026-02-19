4ca406eb2dc6809dea6b0c8c61ccea0874a39877
Lav Kushwaha <lavkumar062@gmail.com>
2026-02-19 17:27:03 +0530

    cloudwatch changes

--- terraform/cloudwatch.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_cloudwatch_log_group" "strapi" {
-  name              = "/ecs/strapi"
-  retention_in_days = 7   # you can change to 14/30 for prod
-
-  tags = {
-    Name = "strapi-ecs-logs"
-    App  = "strapi"
-  }
-}
