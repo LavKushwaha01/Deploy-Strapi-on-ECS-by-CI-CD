5f6544f2a51a31c5563005bf2884709dcd5505e8
Lav kushwaha <152052398+LavKushwaha01@users.noreply.github.com>
2026-02-25 17:05:41 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -14,8 +14,8 @@
     ]
   })
 }
-*/
+
 resource "aws_iam_role_policy_attachment" "codedeploy_policy" {
   role       = aws_iam_role.codedeploy_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRoleForECS"
-}
+}*/
